851a879c1ffa1e41ebebd989a9efb316a54a93cd
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-05-31 15:50:53 -0600

    feat: Add Tailwind plugins to import highlighter.js styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,20 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
+import hljsLight from "react-syntax-highlighter/dist/esm/styles/hljs/nord";
+import hljsDark from "react-syntax-highlighter/dist/esm/styles/hljs/atom-one-dark-reasonable";
+
+const hljsLightClasses = Object.entries(hljsLight).reduce(
+  (acc, [key, value]) => {
+    acc[`& .${key}`] = value;
+    return acc;
+  },
+  {}
+);
+
+const hljsDarkClasses = Object.entries(hljsDark).reduce((acc, [key, value]) => {
+  acc[`& .${key}`] = value;
+  return acc;
+}, {});
 
 const config: Config = {
   content: [
@@ -15,6 +31,13 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addComponents }) => {
+      addComponents({
+        ".hljs-codeblock": hljsLightClasses,
+        ".hljs-codeblock-dark": hljsDarkClasses,
+      });
+    }),
+  ],
 };
 export default config;
