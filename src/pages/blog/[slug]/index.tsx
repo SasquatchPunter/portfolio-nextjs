b54fff3395c299ddb3709f85d86826a0126210ef
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-05-31 10:13:54 -0600

    feat: Add static props and static paths to dynamic blog/[slug] route

--- src/pages/blog/[slug]/index.tsx
+++ src/pages/blog/[slug]/index.tsx
@@ -1,3 +1,47 @@
-export default function BlogPostPage() {
-  return <h1 className="text-4xl">Blog Post Page</h1>;
+import client from "@tina/__generated__/client";
+import { InferGetStaticPropsType } from "next";
+
+export const getStaticPaths = async () => {
+  const entries =
+    (await client.queries.blogConnection()).data.blogConnection.edges || [];
+  const paths = entries
+    .filter((entry) => entry?.node)
+    .map((entry) => ({
+      params: {
+        slug: entry!.node?._sys.filename,
+      },
+    }));
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async ({
+  params,
+}: {
+  params: { slug: string };
+}) => {
+  const entry = (
+    await client.queries.blogConnection()
+  ).data.blogConnection.edges!.find(
+    (entry) => entry?.node?._sys.filename === params.slug
+  );
+
+  const post = await client.queries.blog({
+    relativePath: entry!.node!._sys.relativePath,
+  });
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+export default function BlogPostPage({
+  post,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <>
+      <h1 className="text-4xl">Blog Post Page</h1>
+      <h3>{post.data.blog.title}</h3>
+    </>
+  );
 }
