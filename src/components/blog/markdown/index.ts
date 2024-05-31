7d3a53d4929ef15636d2fae9f618daa34eb4c179
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-05-31 14:53:46 -0600

    feat: Add markdown components

--- src/components/blog/markdown/index.ts
+++ src/components/blog/markdown/index.ts
@@ -1,4 +1,8 @@
 import { Components } from "tinacms/dist/rich-text";
+
+import BlockQuote from "./BlockQuote";
+import Code from "./Code";
+import CodeBlock from "./Codeblock";
 import H1 from "./H1";
 import H2 from "./H2";
 import H3 from "./H3";
@@ -8,6 +12,9 @@ import H6 from "./H6";
 import P from "./P";
 
 export default {
+  block_quote: BlockQuote,
+  code: Code,
+  code_block: CodeBlock,
   h1: H1,
   h2: H2,
   h3: H3,
