02a45b08d1420e3ad830e17aaed684d43eeb2912
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-05-30 22:49:58 -0600

    build: Add base path var to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath,
 };
 
 export default nextConfig;
