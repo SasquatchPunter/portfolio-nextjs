d5551b7087959793ef58fc0d4fbddf7afb0864af
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-05-31 18:26:24 -0600

    feat: Register languages in react-syntax-highlighter module

--- src/components/blog/markdown/CodeBlock.tsx
+++ src/components/blog/markdown/CodeBlock.tsx
@@ -1,7 +1,18 @@
 import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
 import * as classes from "@utils/components/classes";
 import * as styles from "@utils/components/styles";
-import { Button } from "tinacms";
+
+import javascript from "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
+import java from "react-syntax-highlighter/dist/esm/languages/hljs/java";
+import css from "react-syntax-highlighter/dist/esm/languages/hljs/css";
+import typescript from "react-syntax-highlighter/dist/esm/languages/hljs/typescript";
+
+import CopyIcon from "@icons/copy-outline.svg";
+
+SyntaxHighlighter.registerLanguage("javascript", javascript);
+SyntaxHighlighter.registerLanguage("java", java);
+SyntaxHighlighter.registerLanguage("css", css);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
 
 function CodeTag(props: any) {
   const className = "";
@@ -35,10 +46,13 @@ interface TopBarProps {
 }
 function TopBar({ lang, copyText }: TopBarProps) {
   return (
-    <div className="flex justify-between p-2 rounded-t-md">
+    <div className="flex justify-between py-2 px-4 bg-gray-600">
       <span>{lang}</span>
-      <button onClick={() => navigator.clipboard.writeText(copyText)}>
-        Copy
+      <button
+        className="w-6 h-auto"
+        onClick={() => navigator.clipboard.writeText(copyText)}
+      >
+        <CopyIcon />
       </button>
     </div>
   );
@@ -46,7 +60,7 @@ function TopBar({ lang, copyText }: TopBarProps) {
 
 export default function CodeBlock(props: any) {
   return (
-    <div>
+    <div className="overflow-hidden rounded-md border-2 border-gray-500">
       <TopBar lang={props.lang} copyText={props.value} />
       <div className="hljs-codeblock">
         <SyntaxHighlighter
