789e1e001fbb2f341400afe7fe6b4d4ffd9f299c
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-05-31 18:23:57 -0600

    feat: Add @svgr module as webpack loader plugin

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const nextConfig = {
   reactStrictMode: true,
   basePath,
   output: "export",
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  transpilePackages: ["react-syntax-highlighter"],
 };
 
 export default nextConfig;
