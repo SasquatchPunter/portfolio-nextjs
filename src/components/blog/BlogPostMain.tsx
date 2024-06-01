8654a739e939925e0350bce18f2a7ea9f7421ffd
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-01 16:44:25 -0600

    feat(BlogPostMain.tsx): Add styles for post margin spacing

--- src/components/blog/BlogPostMain.tsx
+++ src/components/blog/BlogPostMain.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 export default function BlogPostMain({ body }: Props) {
   return (
-    <main className="m-4 flex flex-col gap-8">
+    <main className="m-4 md:w-3/4 md:m-auto lg:w-1/2 flex flex-col gap-8">
       <TinaMarkdown content={body} components={components} />
     </main>
   );
