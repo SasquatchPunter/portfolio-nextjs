05b6df2f9cfe582d1f04f6e16c42169fbfe8d3c7
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-06 20:56:56 -0600

    feat: Add blog list to main blog page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,33 @@
-export default function BlogPage() {
-  return <h1 className="text-4xl">Blog Page</h1>;
+import BlogList from "@/components/blog/BlogList";
+import client from "@tina/__generated__/client";
+import type {
+  MinBlogConnectionAllQuery,
+  MinBlogConnectionPublishedQuery,
+} from "@tina/__generated__/types";
+
+const ENV = process.env.NODE_ENV;
+
+export async function getStaticProps() {
+  const { data } =
+    ENV === "production"
+      ? await client.queries.minBlogConnectionPublished()
+      : await client.queries.minBlogConnectionAll();
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+interface Props {
+  data: MinBlogConnectionAllQuery | MinBlogConnectionPublishedQuery;
+}
+export default function BlogPage({ data }: Props) {
+  const posts = data.blogConnection.edges || [];
+  return (
+    <>
+      <h1 className="text-4xl">Blog Page</h1>
+      <BlogList posts={posts} />
+    </>
+  );
 }
