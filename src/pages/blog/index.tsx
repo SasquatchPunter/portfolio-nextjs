b99fe8dbcccb5a382279cfb2ed3e576905581279
SasquatchPunter <jeremyrobertelliott@gmail.com>
2024-06-10 16:45:23 -0600

    refactor: Use cleanConnectionEdges helper

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -23,7 +23,8 @@ interface Props {
   data: MinBlogConnectionAllQuery | MinBlogConnectionPublishedQuery;
 }
 export default function BlogPage({ data }: Props) {
-  const posts = data.blogConnection.edges || [];
+  const posts = cleanConnectionEdges(data.blogConnection.edges);
+
   return (
     <>
       <h1 className="text-4xl">Blog Page</h1>
